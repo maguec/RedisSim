b64dc83372df3e7b4c7d2c33f775f83e7ff5436e
Chris Mague <github@mague.com>
2023-11-11 07:16:39 -0800

    add a slow csv loader, will need to increase performance here

---
+++ cmd/loadcsv.go
@@ -0,0 +1,44 @@
+/*
+Copyright © 2023 Chris Mague github@mague.com
+*/
+package cmd
+
+import (
+	"log"
+
+	"github.com/maguec/RedisSim/simredis"
+	"github.com/maguec/RedisSim/utils"
+	"github.com/spf13/cobra"
+)
+
+var csvfile, keyfield string
+var batchsize int
+var csvstatsHide bool
+
+// loadcsvCmd represents the loadcsv command
+var loadcsvCmd = &cobra.Command{
+	Use:   "loadcsv",
+	Short: "Load a CSV file into Redis",
+	Long: `Takes a CSV file and loads it into a Redis hash datas structure.
+User needs to define the CSV field to load in as the key.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		conf := simredis.RedisConf(server, password, clients, port)
+		cluster := simredis.ClusterClient(conf, ctx)
+		err := cluster.Ping(ctx).Err()
+		if err != nil {
+			log.Panic("Unable to connect to cluster: ", err.Error())
+		}
+		err = utils.CSVwrite(conf, ctx, clients, rps, csvfile, keyfield, csvstatsHide)
+		if err != nil {
+			log.Panic("Unable to connect to cluster: ", err.Error())
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(loadcsvCmd)
+	loadcsvCmd.Flags().StringVar(&csvfile, "csv-file", "", "CSV file to load into Redis")
+	loadcsvCmd.Flags().StringVar(&keyfield, "key-field", "", "CSV field to set as the key name")
+	loadcsvCmd.Flags().IntVar(&batchsize, "batch-size", 300, "Size of write batches")
+	loadcsvCmd.Flags().BoolVarP(&csvstatsHide, "stats-hide", "x", false, "Hide statistics")
+}
---
+++ utils/csvwrite.go
@@ -0,0 +1,126 @@
+package utils
+
+import (
+	"context"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+	"slices"
+	"sync"
+	"time"
+
+	"github.com/go-redis/redis/v9"
+	"github.com/jamiealquiza/tachymeter"
+	"github.com/maguec/RedisSim/simredis"
+	"github.com/maguec/metermaid"
+	"go.uber.org/ratelimit"
+)
+
+// TODO: this needs to be batched, have a prefix and rate limiting etc, but for now works slowly
+func CSVwrite(conf *redis.ClusterOptions, ctx context.Context, clients, rps int, csvfile, keyfield string, hide bool) error {
+	rows, _, err := csv2map(csvfile, keyfield)
+	if err != nil {
+		return err
+	}
+	client := simredis.ClusterClient(conf, ctx)
+	for _, row := range rows {
+		_, e := client.HSet(ctx, row[keyfield].(string), row).Result()
+		if e != nil {
+			return e
+		}
+	}
+	return nil
+}
+
+func csv2map(csvfile, keyfield string) ([]map[string]interface{}, []string, error) {
+	rows := []map[string]interface{}{}
+	var headers []string
+	f, err := os.Open(csvfile)
+	if err != nil {
+		return nil, nil, err
+	}
+	r := csv.NewReader(f)
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, nil, err
+		}
+		if headers == nil {
+			headers = record
+		} else {
+			dict := map[string]interface{}{}
+			for i := range headers {
+				dict[headers[i]] = record[i]
+			}
+			rows = append(rows, dict)
+		}
+	}
+	if slices.Contains(headers, keyfield) {
+		return rows, headers, nil
+	} else {
+		return nil, nil, fmt.Errorf("Keyname is not in the headers")
+	}
+}
+
+func csvworker(
+	id int, wg *sync.WaitGroup, conf *redis.ClusterOptions,
+	jobs <-chan int,
+	ctx context.Context, size, rps int, rl ratelimit.Limiter,
+	mm *metermaid.Metermaid, tach *tachymeter.Tachymeter,
+) {
+	client := simredis.ClusterClient(conf, ctx)
+	client.Ping(ctx)
+	defer wg.Done()
+	for {
+		if rps > 0 {
+			rl.Take()
+		}
+		select {
+		case job, ok := <-jobs:
+			if !ok {
+				fmt.Printf("Error with thread %d\n", id)
+			}
+			startTime := time.Now()
+			err := client.Set(ctx, fmt.Sprintf("string:%d", job), RandStringBytes(size), 0).Err()
+			if err != nil {
+				fmt.Printf("Error with thread %d and job %d\n", id, job)
+			}
+			tach.AddTime(time.Since(startTime))
+			mm.Add()
+		default:
+			return
+		}
+	}
+}
+
+//func Stringfill(conf *redis.ClusterOptions, size, count, threads, rps int, hide bool) error {
+//	var ctx = context.Background()
+//	txns := make(chan int, count)
+//	for t := 0; t < count; t++ {
+//		txns <- t
+//	}
+//
+//	// rewrite this if the RPS > 0 since creating a ratelimiter with 0 will div by zero
+//	rl := ratelimit.New(1)
+//	if rps > 0 {
+//		rl = ratelimit.New(rps)
+//	}
+//
+//	tach := tachymeter.New(&tachymeter.Config{Size: count})
+//	mm := metermaid.New(&metermaid.Config{Size: count})
+//	wg := new(sync.WaitGroup)
+//	for w := 0; w < threads; w++ {
+//		wg.Add(1)
+//		go fillworker(w, wg, conf, txns, ctx, size, rps, rl, mm, tach)
+//	}
+//	wg.Wait()
+//	if !hide {
+//		ShowStats(tach, mm)
+//	}
+//	return nil
+//}
+//
